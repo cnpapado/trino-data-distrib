07e7df6b2c43dda08fd8a259cae70355d7176190
Constantinos Papadopoulos <14839869+cnpapado@users.noreply.github.com>
2022-12-25 02:35:07 +0200

    Added files for mysql tpcds demo

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # trino-rhino
+This branch is about manually creating the TPC-DS schema in a MySQL db, loading all the data and validating it's testing with the validation queries.
+
+- `run.sh` creates the testing dataset (1GB of input data) as well as 1 variant of each query 
+- `my.sql` creates the db, schema and loads the input data files into the tables   
+
+Both scripts need the `tpcds` folders in the same directory, `tpcds/tools` compiled with `make CC=gcc-9 OS=LINUX`.
---
+++ run.sh
@@ -0,0 +1,29 @@
+cd "/home/c/Documents/Mathimata/9ο ΕΞΑΜΗΝΟ/Big Data/tpcds/tools"
+
+mkdir -p ../../data # create a folder for the input data files
+
+./dsdgen -dir ../../data/ -sc 1 -verbose # generate input data of 1GB size 
+
+sed -i -e 's/^|/\\N|/' -e 's/||/|\\N|/g' -e 's/||/|\\N|/g' -e 's/|$/|/' ../../data/*.dat # replace all empty columns with /N (MySQL only)
+
+split -l$((`wc -l < store_sales.dat`/5)) store_sales.dat store_sales.split.dat -da 10 # store_sales.dat in large so we split it into 10/2=5 files
+
+mkdir -p ../../queries
+
+touch qlist.lst # dsqgen puts all the queries from the templates dir whose name are specified in the -INPUT (qlist) file in one output file
+for i in $(seq 1 1 99)
+do
+echo "query$i.tpl" > qlist.lst # to create each query in a different file, each time write a new template name into the qlist file
+./dsqgen \
+-DIRECTORY ../query_templates \
+-INPUT qlist.lst \
+-VERBOSE Y \
+-QUALIFY Y \
+-SCALE 1 \
+-DIALECT netezza \
+-OUTPUT_DIR ../../queries
+
+mv ../../queries/query_0.sql ../../queries/"query$i.sql" # output of dsqgen for 1 query variant/query is query_0.sql regardless of the query template 
+
+done
+rm qlist.lst
